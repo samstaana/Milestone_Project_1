18de942ff2769a2f406bed676743b69afef4e4ae
KS Durazo <sdurazo.UNLV@gmail.com>
2022-06-18 11:07:19 -0700

    added pseudocode for javascript

---
+++ index.js
@@ -0,0 +1,25 @@
+// select elements
+
+// add questions
+
+// variables
+
+// start quiz functions
+
+// render progress
+
+// counter render
+
+// check answers
+
+// if the answer is correct
+
+// if the answer is wrong
+
+// score
+
+// percentage of correct answers
+
+// images for the final score tally
+
+// invoke the code
