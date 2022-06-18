696755fdb3ee48a44108774b5f9068045122a124
KS Durazo <sdurazo.UNLV@gmail.com>
2022-06-18 11:44:55 -0700

    added questions

--- index.js
+++ index.js
@@ -1,6 +1,113 @@
 // select elements
 
 // add questions
+let questions = [
+    {
+      question: "Where does Michael Scott move to start his new life with Holly?",
+      choiceA: "New York, New York",
+      choiceB: "Los Angeles, California",
+      choiceC: "Boulder, Colorado",
+      choiceD: "Salt Lake City, Utah",
+      correct: "C"
+      },
+    {
+      question: "Which of these is NOT a name of one Angela's cats?",
+      choiceA: "Bandit",
+      choiceB: "Sprinkles",
+      choiceC: "Petals",
+      choiceD: "Oreo",
+      correct: "D"
+    },
+    {
+      question: "At Phyllis' wedding, Michael revealed that her nickname in high school was what?",
+      choiceA: "Perfectly Adequate",
+      choiceB: "Easy Rider",
+      choiceC: "OCD Girl",
+      choiceD: "Belle",
+      correct: "B"
+    },
+    {
+      question: "Michael and Dwight tried to steal clients from which local competing business?",
+      choiceA: "Staples",
+      choiceB: "Pendelton",
+      choiceC: "Catalyst Paper",
+      choiceD: "Prince Family Paper",
+      correct: "D"
+    },
+    {
+      question: "Finish Dwight's security code: “The tea in Nepal is very hot, but the coffee in ______ is much hotter.",
+      choiceA: "Cuba",
+      choiceB: "Cebu",
+      choiceC: "Peru",
+      choiceD: "Brazil",
+      correct: "C"
+    },
+    {
+      question: "Which Harry Potter book did Dwight say he'd take to a desert island?",
+      choiceA: "Harry Potter and the Prisoner of Azkaban",
+      choiceB: "Harry Potter and the Order of the Phoenix",
+      choiceC: "Harry Potter and the Half-Blood Prince",
+      choiceD: "Harry Potter and the Sorcerer's Stone",
+      correct: "A"
+    },
+    {
+      question: "While playing who would you do in the office, Michael says he’d have sex with which coworker?",
+      choiceA: "Pam",
+      choiceB: "Angela",
+      choiceC: "Ryan",
+      choiceD: "Danny",
+      correct: "C"
+    },
+    {
+      question: "Who won the last 'Hottest in the Office Dundie Award'?",
+      choiceA: "Ryan Howard",
+      choiceB: "Pam Beesly",
+      choiceC: "Danny Cordray",
+      choiceD: "Michael Scott",
+      correct: "C"
+    },
+    {
+      question: "What does Dwight say is the scariest animal?",
+      choiceA: "Black Bear",
+      choiceB: "Lions",
+      choiceC: "Sharks",
+      choiceD: "Box Jellyfish",
+      correct: "D"
+    },
+    {
+      question: "Why is Michael afraid to move to Colorado?",
+      choiceA: "He's going to miss his Dunder Mifflin family.",
+      choiceB: "The TV channels will be in the wrong order.",
+      choiceC: "He'll never see Ryan again.",
+      choiceD: "He doesn't know anyone is Colorado",
+      correct: "B"
+    },
+    {
+      question: "What is the title of the movie Michael Scott makes?",
+      choiceA: "Level One: Midnight",
+      choiceB: "Michael Scorn",
+      choiceC: "Nightmare in  Scranton",
+      choiceD: "Threat Level Midnight",
+      correct: "D"
+    },
+    {
+      question: "When Dwight is explaining his perfect crime, what is the name of the girl?",
+      choiceA: "Tawny",
+      choiceB: "Taylor",
+      choiceC: "Tiffany",
+      choiceD: "Tammy",
+      correct: "C"
+    },
+    {
+      question: "In the “Diversity Day” episode, what famous comedian’s stand up routine does Michael imitate?",
+      choiceA: "Chris Rock",
+      choiceB: "Dave Chappelle",
+      choiceC: "Louis C.K.",
+      choiceD: "Ricky Gervis",
+      correct: "A"
+    },
+    
+]
 
 // variables
 
